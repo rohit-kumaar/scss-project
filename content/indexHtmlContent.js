e2c55dc8e8726f523f1b132aa836aca706b62368
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2025-05-07 16:31:52 +0530

    ✨ update indexHtmlContent structure for improved readability and organization; enhance styleScssContent with flow comments

--- content/indexHtmlContent.js
+++ content/indexHtmlContent.js
@@ -1,83 +1,56 @@
 export const indexHtmlContent = `<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="author" content="Rohit Kumar" />
-    <meta name="description" content="Your Project Description" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Your Project Title</title>
 
-    <!-- Favicon Start  -->
-    <link rel="icon" href="src/icons/favicon.ico" type="image/x-icon" />
-    <link
-      rel="android-chrome"
-      sizes="192x192"
-      href="src/icons/android-chrome-192x192.png"
-    />
-    <link
-      rel="android-chrome"
-      sizes="512x192"
-      href="src/icons/android-chrome-512x512.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="src/icons/apple-touch-icon.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="src/icons/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="src/icons/favicon-16x16.png"
-    />
-    <link rel="manifest" href="src/icons/site.webmanifest" />
-    <!-- Favicon End  -->
+<head>
+  <meta charset="UTF-8" />
+  <meta name="author" content="Rohit Kumar" />
+  <meta name="description" content="Your Project Description" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Your Project Title</title>
 
-    <!-- Custom CSS Start  -->
-    <link rel="stylesheet" href="src/css/style.css" />
-    <!-- Custom CSS End  -->
-  </head>
+  <!-- Start : Favicon -->
+  <link rel="icon" href="src/icons/favicon.ico" type="image/x-icon" />
+  <link rel="android-chrome" sizes="192x192" href="src/icons/android-chrome-192x192.png" />
+  <link rel="android-chrome" sizes="512x192" href="src/icons/android-chrome-512x512.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="src/icons/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="src/icons/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="src/icons/favicon-16x16.png" />
+  <link rel="manifest" href="src/icons/site.webmanifest" />
+  <!-- End   : Favicon -->
 
-  <body>
-    <div class="container">
-      <div class="wrapper">
-        <h1
-          class="bg-primary-700 text-primary-50"
-          style="
-            display: grid;
-            place-items: center;
-            height: 100dvh;
-            padding: 30px;
-            font-size: clamp(0.6875rem, -0.0582rem + 3.7285vw, 3.125rem);
-          "
-        >
-          Welcome! This is your SCSS project.
-        </h1>
-      </div>
-    </div>
+  <!-- Start : Custom CSS -->
+  <link rel="stylesheet" href="src/css/style.css" />
+  <!-- End   : Custom CSS -->
+</head>
 
-    <!-- Owl Carousel Start -->
-    <!-- <script src="src/js/jquery/jquery.min.js"></script> -->
-    <!-- <script src="src/js/owl_carousel/owl.carousel.min.js"></script> -->
-    <!-- Owl Carousel End  -->
+<body>
+  <!-- Start : main -->
+  <main class="container">
+    <h1 class="bg-primary-700 text-primary-50" style="
+          display: grid;
+          place-items: center;
+          height: 100dvh;
+          padding: 30px;
+          font-size: clamp(0.6875rem, -0.0582rem + 3.7285vw, 3.125rem);
+        ">
+      Welcome! This is your SCSS project.
+    </h1>
+  </main>
+  <!-- End   : main -->
 
-    <!-- Bootstrap  JS Start -->
-    <!-- <script src="src/js/bootstrap/bootstrap.bundle.min.js"></script> -->
-    <!-- Bootstrap  JS End  -->
+  <!-- Start : Owl Carousel -->
+  <!-- <script src="src/js/jquery/jquery.min.js"></script> -->
+  <!-- <script src="src/js/owl_carousel/owl.carousel.min.js"></script> -->
+  <!-- End   : Owl Carousel -->
 
-    <!-- Custom JS Start -->
-    <script src="src/js/index.js"></script>
-    <!-- Custom JS End -->
-  </body>
-</html>
+  <!-- Start : Bootstrap  JS -->
+  <!-- <script src="src/js/bootstrap/bootstrap.bundle.min.js"></script> -->
+  <!-- End   : Bootstrap  JS -->
 
+  <!-- Start : Custom JS -->
+  <script src="src/js/index.js"></script>
+  <!-- End   : Custom JS -->
+</body>
 
-
-`;
+</html>`;
--- content/styleScssContent.js
+++ content/styleScssContent.js
@@ -1,9 +1,14 @@
-export const styleScssContent = `@charset 'utf-8';
-// Don't change the import folder links
+export const styleScssContent = `/* --------------------------------*/
+/* Start : Don't Change The Flow   */
+/* --------------------------------*/
+@charset 'utf-8';
 @use "vendors/__vendors-dir.scss";
 @use "utilities/__utilities-dir.scss";
 @use "base/__base-dir.scss";
 @use "components/__components-dir.scss";
 @use "layout/__layout-dir.scss";
 @use "pages/__pages-dir.scss";
+/* --------------------------------*/
+/* End   : Don't Change The Flow   */
+/* --------------------------------*/
 `;
